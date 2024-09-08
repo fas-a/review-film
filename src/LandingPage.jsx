ad6aae2278945bc713d12cb6c55a0558955fb235
fas-a <abulkhoirfaris0@gmail.com>
2024-09-08 17:43:15 +0700

    fix error landing page

---
+++ src/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Carousel from './components/Carousel';
+import SweepCard from './components/SweepCard';
+
+function LandingPage() {
+  return (
+    <div>
+      <Carousel />
+      <SweepCard />
+    </div>
+  );
+}
+
+export default LandingPage;
