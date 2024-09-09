f393ec601426d65248eaf6d956749fd3d32ab0e1
salsabilk <salsabilkhoirunisa271@gmail.com>
2024-09-09 10:27:51 +0700

    cms, login, regis

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './components/Header';
-import LandingPage from './LandingPage';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './components/tailwind.output.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import LandingPage from "./LandingPage";
+import Login from "./components/Login";
+import CreateAccount from "./components/CreateAccount";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./components/tailwind.output.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <LandingPage />
+            </>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-account" element={<CreateAccount />} />
       </Routes>
     </Router>
   </React.StrictMode>
